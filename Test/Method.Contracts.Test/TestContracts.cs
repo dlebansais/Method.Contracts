684d337d1e85011f9391b73a119b6aed71f7309d
David Le Bansais <dlebansais@gmail.com>
2023-12-24 23:52:05 +0100

    Added some tests.

---
+++ Test/Method.Contracts.Test/TestContracts.cs
@@ -0,0 +1,37 @@
+﻿namespace Contracts.Test;
+
+using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+[TestFixture]
+public class TestContracts
+{
+    [Test]
+    public void TestAssertNotNull()
+    {
+#if DEBUG
+        TestAssertNotNull_Debug();
+#else
+        TestAssertNotNull_Release();
+#endif
+    }
+
+    private static void TestAssertNotNull_Debug()
+    {
+        DebugTraceListener Listener = new();
+        Trace.Listeners.Clear();
+        Trace.Listeners.Add(Listener);
+
+        const string? NullString = null;
+        Contract.RequireNotNull<string>(NullString, out _);
+
+        Assert.That(Listener.IsAssertTriggered, Is.True);
+    }
+
+    private static void TestAssertNotNull_Release()
+    {
+        const string? NullString = null;
+        Assert.Throws<ArgumentNullException>(() => Contract.RequireNotNull<string>(NullString, out _));
+    }
+}
---
+++ Test/Method.Contracts.Test/Tools/DebugTraceListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace Contracts.Test;
+
+public class DebugTraceListener : TraceListener
+{
+    public bool IsAssertTriggered { get; private set; }
+
+    public override void Write(string? message)
+    {
+    }
+
+    public override void WriteLine(string? message)
+    {
+    }
+
+    public override void Fail(string? message)
+    {
+        IsAssertTriggered = true;
+    }
+
+    public override void Fail(string? message, string? detailMessage)
+    {
+        IsAssertTriggered = true;
+    }
+}
