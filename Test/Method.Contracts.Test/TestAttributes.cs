fd4e6f0a6e54dece01ccb7fd49c0dc4a3c73adfa
David Le Bansais <dlebansais@gmail.com>
2024-04-29 20:31:05 +0200

    Added a test for code coverage.

---
+++ Test/Method.Contracts.Test/TestAttributes.cs
@@ -0,0 +1,24 @@
+﻿namespace Contracts.Test;
+
+using NUnit.Framework;
+
+[TestFixture]
+public class TestAttributes
+{
+    [Test]
+    public void TestSampleMethod()
+    {
+        SampleMethod(string.Empty);
+    }
+
+    [Access("public")]
+    [RequireNotNull("text")]
+    [Require("text.Length >= 0")]
+    [Ensure("text.Length >= 0")]
+    public void SampleMethod(string text)
+    {
+        SampleField++;
+    }
+
+    public int SampleField { get; set; }
+}
