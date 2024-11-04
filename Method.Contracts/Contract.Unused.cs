2a05070053cf789b2b09665255b759cf9b33dacd
David Le Bansais <dlebansais@gmail.com>
2024-11-04 13:32:18 +0100

    Added support for unused value types and nullable value types.

--- Method.Contracts/Contract.Unused.cs
+++ Method.Contracts/Contract.Unused.cs
@@ -15,4 +15,30 @@ public static partial class Contract
     {
         result = default!;
     }
+
+    /// <summary>
+    /// Provides a value for variables that should be uninitialized.
+    /// </summary>
+    /// <typeparam name="T">The value type.</typeparam>
+    /// <param name="result">The unused value.</param>
+    public static void Unused<T>(out T? result)
+        where T : struct
+    {
+        result = null;
+    }
+
+    /// <summary>
+    /// Provides a value for variables that should be uninitialized.
+    /// </summary>
+    /// <typeparam name="T">The value type.</typeparam>
+    /// <param name="result">The unused value.</param>
+    /// <param name="_">Unused.</param>
+    public static void Unused<T>(out T result,
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
+                                 T? _ = null)
+#pragma warning restore SA1313 // Parameter names should begin with lower-case letter
+        where T : struct
+    {
+        result = default;
+    }
 }
--- Test/Method.Contracts.Test/TestUnused.cs
+++ Test/Method.Contracts.Test/TestUnused.cs
@@ -6,10 +6,26 @@ using NUnit.Framework;
 public class TestUnused
 {
     [Test]
-    public void Test()
+    public void TestClass()
     {
         Contract.Unused(out string UnusedString);
 
         Assert.That(UnusedString, Is.Null);
+    }
+
+    [Test]
+    public void TestNullable()
+    {
+        Contract.Unused(out int? UnusedInt);
+
+        Assert.That(UnusedInt, Is.Null);
+    }
+
+    [Test]
+    public void TestStruct()
+    {
+        Contract.Unused(out int UnusedInt);
+
+        Assert.That(UnusedInt, Is.EqualTo(0));
     }
 }
