e6bd6c9f987ebb2ae4f33553f3df940cd35ec6f7
David Le Bansais <dlebansais@gmail.com>
2025-08-17 11:29:47 +0200

    Fixed GitCommitId.

--- updatecommit.bat
+++ updatecommit.bat
@@ -1,9 +1,9 @@
-if exist "%~1packages\GitCommitId*" goto run1
+if exist "%~1packages\dlebansais.GitCommitId*" goto run1
 if exist "%~1..\Version Tools\GitCommitId.exe" goto run2
 goto error
 
 :run1
-for /D %%F in (%~1packages\GitCommitId*) do "%%F\lib\net48\GitCommitId.exe" %2 -u
+for /D %%F in (%~1packages\dlebansais.GitCommitId*) do "%%F\lib\net48\GitCommitId.exe" %2 -u
 goto end
 
 :run2
