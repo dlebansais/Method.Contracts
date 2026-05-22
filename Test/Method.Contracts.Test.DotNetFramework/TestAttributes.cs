a391197f45591c9ff3dccd2e2b02b15295a64dd9
David Le Bansais <dlebansais@gmail.com>
2026-05-22 21:24:34 +0200

    CI test.

---
+++ Test/Method.Contracts.Test.DotNetFramework/TestAttributes.cs
@@ -0,0 +1,145 @@
+﻿namespace Contracts.Test;
+
+using NUnit.Framework;
+
+[TestFixture]
+internal class TestAttributes
+{
+    [TestCase(TestName = "Access attribute")]
+    public void TestAccessAttribute()
+    {
+        AccessAttribute Access1 = new("public");
+        Assert.That(Access1.Specifiers, Has.Length.EqualTo(1));
+
+        AccessAttribute Access2 = new("public", "virtual");
+        Assert.That(Access2.Specifiers, Has.Length.EqualTo(2));
+
+        AccessAttribute Access3 = new("public", "virtual", "unchecked");
+        Assert.That(Access3.Specifiers, Has.Length.EqualTo(3));
+
+        AccessAttribute Access4 = new("public", "virtual", "unchecked", "partial");
+        Assert.That(Access4.Specifiers, Has.Length.EqualTo(4));
+
+        AccessAttribute Access5 = new("private", "protected", "virtual", "unchecked", "partial");
+        Assert.That(Access5.Specifiers, Has.Length.EqualTo(5));
+    }
+
+    [TestCase(TestName = "RequireNotNull attribute")]
+    public void TestRequireNotNullAttribute()
+    {
+        RequireNotNullAttribute RequireNotNull = new("text");
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(RequireNotNull.ArgumentNames, Has.Length.EqualTo(1));
+            Assert.That(RequireNotNull.Type, Is.EqualTo(string.Empty));
+            Assert.That(RequireNotNull.Name, Is.EqualTo(string.Empty));
+            Assert.That(RequireNotNull.AliasName, Is.EqualTo(string.Empty));
+        }
+
+        const string Type = "Type";
+        const string Name = "Name";
+        const string AliasName = "AliasName";
+
+        RequireNotNullAttribute RequireNotNullWithType = new("text") { Type = Type };
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(RequireNotNullWithType.ArgumentNames, Has.Length.EqualTo(1));
+            Assert.That(RequireNotNullWithType.Type, Is.EqualTo(Type));
+            Assert.That(RequireNotNullWithType.Name, Is.EqualTo(string.Empty));
+            Assert.That(RequireNotNullWithType.AliasName, Is.EqualTo(string.Empty));
+        }
+
+        RequireNotNullAttribute RequireNotNullWithName = new("text") { Name = Name };
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(RequireNotNullWithName.ArgumentNames, Has.Length.EqualTo(1));
+            Assert.That(RequireNotNullWithName.Type, Is.EqualTo(string.Empty));
+            Assert.That(RequireNotNullWithName.Name, Is.EqualTo(Name));
+            Assert.That(RequireNotNullWithName.AliasName, Is.EqualTo(string.Empty));
+        }
+
+        RequireNotNullAttribute RequireNotNullWithAliasName = new("text") { AliasName = AliasName };
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(RequireNotNullWithAliasName.ArgumentNames, Has.Length.EqualTo(1));
+            Assert.That(RequireNotNullWithAliasName.Type, Is.EqualTo(string.Empty));
+            Assert.That(RequireNotNullWithAliasName.Name, Is.EqualTo(string.Empty));
+            Assert.That(RequireNotNullWithAliasName.AliasName, Is.EqualTo(AliasName));
+        }
+
+        RequireNotNullAttribute RequireNotNullWithAliasTypeAndName = new("text") { Type = Type, Name = Name, AliasName = AliasName };
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(RequireNotNullWithAliasTypeAndName.ArgumentNames, Has.Length.EqualTo(1));
+            Assert.That(RequireNotNullWithAliasTypeAndName.Type, Is.EqualTo(Type));
+            Assert.That(RequireNotNullWithAliasTypeAndName.Name, Is.EqualTo(Name));
+            Assert.That(RequireNotNullWithAliasTypeAndName.AliasName, Is.EqualTo(AliasName));
+        }
+
+        RequireNotNullAttribute RequireNotNull2 = new("text1", "text2");
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(RequireNotNull2.ArgumentNames, Has.Length.EqualTo(2));
+            Assert.That(RequireNotNull2.Type, Is.EqualTo(string.Empty));
+            Assert.That(RequireNotNull2.Name, Is.EqualTo(string.Empty));
+            Assert.That(RequireNotNull2.AliasName, Is.EqualTo(string.Empty));
+        }
+    }
+
+    [TestCase(TestName = "Require attribute")]
+    public void TestRequireAttribute()
+    {
+        RequireAttribute Require = new("true");
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(Require.Expressions, Has.Length.EqualTo(1));
+            Assert.That(Require.DebugOnly, Is.False);
+        }
+
+        RequireAttribute RequireDebugOnly = new("true") { DebugOnly = true };
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(RequireDebugOnly.Expressions, Has.Length.EqualTo(1));
+            Assert.That(RequireDebugOnly.DebugOnly, Is.True);
+        }
+
+        RequireAttribute Require2 = new("true", "true");
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(Require2.Expressions, Has.Length.EqualTo(2));
+            Assert.That(Require2.DebugOnly, Is.False);
+        }
+    }
+
+    [TestCase(TestName = "Ensure attribute")]
+    public void TestEnsureAttribute()
+    {
+        EnsureAttribute Ensure = new("true");
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(Ensure.Expressions, Has.Length.EqualTo(1));
+            Assert.That(Ensure.DebugOnly, Is.False);
+        }
+
+        EnsureAttribute EnsureDebugOnly = new("true") { DebugOnly = true };
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(EnsureDebugOnly.Expressions, Has.Length.EqualTo(1));
+            Assert.That(EnsureDebugOnly.DebugOnly, Is.True);
+        }
+
+        EnsureAttribute Ensure2 = new("true", "true");
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(Ensure2.Expressions, Has.Length.EqualTo(2));
+            Assert.That(Ensure2.DebugOnly, Is.False);
+        }
+    }
+
+    [TestCase(TestName = "InitializeWith attribute")]
+    public void TestInitializeWithAttribute()
+    {
+        InitializeWithAttribute InitializeWith = new("text");
+        Assert.That(InitializeWith.MethodName, Is.EqualTo("text"));
+    }
+}
---
+++ Test/Method.Contracts.Test.DotNetFramework/TestMapAsyncAction.cs
@@ -0,0 +1,154 @@
+﻿namespace Contracts.Test;
+
+using System;
+using System.Collections.Generic;
+
+#if DEBUG
+using System.Diagnostics;
+#endif
+
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+[TestFixture]
+internal class TestMapAsyncAction
+{
+    private enum TestEnum
+    {
+        None = 0,
+        Some = 1,
+        More = 2,
+    }
+
+    [TestCase(TestName = "Map success (async action)")]
+    public async Task TestSuccess()
+    {
+        const int NoneValue = 10;
+        int Result = 0;
+
+        Dictionary<TestEnum, Func<Task>> Dictionary = new()
+        {
+            { TestEnum.None, async () => await Task.Run(() => { Result = NoneValue; }).ConfigureAwait(false) },
+            { TestEnum.Some, async () => await Task.Run(() => { Result = 20; }).ConfigureAwait(false) },
+            { TestEnum.More, async () => await Task.Run(() => { Result = 30; }).ConfigureAwait(false) },
+        };
+
+#if DEBUG
+        DebugTraceListener Listener = new();
+        Trace.Listeners.Clear();
+        Trace.Listeners.Add(Listener);
+
+        await Contract.MapAsync(TestEnum.None, Dictionary).ConfigureAwait(false);
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(Listener.IsAssertTriggered, Is.False);
+            Assert.That(Result, Is.EqualTo(NoneValue));
+        }
+#else
+        Assert.DoesNotThrowAsync(async () => await Contract.MapAsync(TestEnum.None, Dictionary).ConfigureAwait(false));
+        Assert.That(Result, Is.EqualTo(NoneValue));
+#endif
+
+        await Task.CompletedTask.ConfigureAwait(false);
+    }
+
+    [TestCase(TestName = "Map failure with bad value (async action)")]
+    public async Task TestFailureBadValue()
+    {
+        int Result = 0;
+        Dictionary<TestEnum, Func<Task>> Dictionary = new()
+        {
+            { TestEnum.None, async () => await Task.Run(() => { Result = 10; }).ConfigureAwait(false) },
+            { TestEnum.Some, async () => await Task.Run(() => { Result = 20; }).ConfigureAwait(false) },
+            { TestEnum.More, async () => await Task.Run(() => { Result = 30; }).ConfigureAwait(false) },
+        };
+
+#if DEBUG
+        DebugTraceListener Listener = new();
+        Trace.Listeners.Clear();
+        Trace.Listeners.Add(Listener);
+
+        await Contract.MapAsync((TestEnum)int.MaxValue, Dictionary).ConfigureAwait(false); int lineNumber = DebugTraceListener.LineNumber(); const string expressionText = "(TestEnum)int.MaxValue"; const int IntValue = int.MaxValue;
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(Listener.IsAssertTriggered, Is.True);
+            Assert.That(Listener.IsOnlyOneMessage, Is.True);
+            Assert.That(Listener.LastMessage, Is.EqualTo($"Enum '{expressionText}' with value {IntValue} not in dictionary, line {lineNumber}"));
+            Assert.That(Result, Is.Zero);
+        }
+#else
+        BrokenContractException Exception = Assert.ThrowsAsync<BrokenContractException>(async () => await Contract.MapAsync((TestEnum)int.MaxValue, Dictionary).ConfigureAwait(false)); int lineNumber = DebugTraceListener.LineNumber(); const string expressionText = "(TestEnum)int.MaxValue"; const int IntValue = int.MaxValue;
+
+        Assert.That(Exception.Message, Is.EqualTo($"Enum '{expressionText}' with value {IntValue} not in dictionary, line {lineNumber}"));
+        Assert.That(Result, Is.Zero);
+#endif
+
+        await Task.CompletedTask.ConfigureAwait(false);
+    }
+
+    [TestCase(TestName = "Map failure with bad dictionary (async action)")]
+    public async Task TestFailureBadDictionary()
+    {
+        int Result = 0;
+        Dictionary<TestEnum, Func<Task>> Dictionary = new()
+        {
+            { TestEnum.Some, async () => await Task.Run(() => { Result = 20; }).ConfigureAwait(false) },
+            { TestEnum.More, async () => await Task.Run(() => { Result = 30; }).ConfigureAwait(false) },
+        };
+
+#if DEBUG
+        DebugTraceListener Listener = new();
+        Trace.Listeners.Clear();
+        Trace.Listeners.Add(Listener);
+
+        await Contract.MapAsync(TestEnum.More, Dictionary).ConfigureAwait(false); int lineNumber = DebugTraceListener.LineNumber(); const string dictionaryText = "Dictionary";
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(Listener.IsAssertTriggered, Is.True);
+            Assert.That(Listener.IsOnlyOneMessage, Is.True);
+            Assert.That(Listener.LastMessage, Is.EqualTo($"Invalid dictionary, line {lineNumber}: {dictionaryText}"));
+            Assert.That(Result, Is.Zero);
+        }
+#else
+        BrokenContractException Exception = Assert.ThrowsAsync<BrokenContractException>(async () => await Contract.MapAsync(TestEnum.More, Dictionary).ConfigureAwait(false)); int lineNumber = DebugTraceListener.LineNumber(); const string dictionaryText = "Dictionary";
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(Exception.Message, Is.EqualTo($"Invalid dictionary, line {lineNumber}: {dictionaryText}"));
+            Assert.That(Result, Is.Zero);
+        }
+#endif
+
+        await Task.CompletedTask.ConfigureAwait(false);
+    }
+
+    [TestCase(TestName = "Map failure with null dictionary (async action)")]
+    public async Task TestNullDictionary()
+    {
+        const Dictionary<TestEnum, Func<Task>> Dictionary = null!;
+
+#if DEBUG
+        DebugTraceListener Listener = new();
+        Trace.Listeners.Clear();
+        Trace.Listeners.Add(Listener);
+
+        await Contract.MapAsync(TestEnum.None, Dictionary).ConfigureAwait(false); int lineNumber = DebugTraceListener.LineNumber(); const string dictionaryText = "Dictionary";
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(Listener.IsAssertTriggered, Is.True);
+            Assert.That(Listener.IsOnlyOneMessage, Is.True);
+            Assert.That(Listener.LastMessage, Is.EqualTo($"Invalid null dictionary, line {lineNumber}: {dictionaryText}"));
+        }
+#else
+        BrokenContractException Exception = Assert.ThrowsAsync<BrokenContractException>(async () => await Contract.MapAsync(TestEnum.None, Dictionary).ConfigureAwait(false)); int lineNumber = DebugTraceListener.LineNumber(); const string dictionaryText = "Dictionary";
+
+        Assert.That(Exception.Message, Is.EqualTo($"Invalid null dictionary, line {lineNumber}: {dictionaryText}"));
+#endif
+
+        await Task.CompletedTask.ConfigureAwait(false);
+    }
+}
