e371fea686a9a377d8b301f6da70b707a3fe49a6
David Le Bansais <dlebansais@gmail.com>
2026-06-11 19:35:56 +0200

    CI test.

--- test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-setlocal
-
-nuget install dlebansais.Packager -Version 2.2.1 -OutputDirectory packages
-
-.\packages\dlebansais.Packager.2.2.1\lib\net10.0-windows7.0\Packager.exe
-echo ERRORLEVEL: %ERRORLEVEL%
